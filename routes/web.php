6871b73f04e08a33d807e922b5416ee60870d38c
poljak07 <acmeta4@gmail.com>
2024-08-27 22:17:34 +0200

    routes optimized

--- routes/web.php
+++ routes/web.php
@@ -1,122 +1,64 @@
 <?php
 
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TagController;
+use App\Http\Controllers\{
+    AboutController,
+    ArticleController,
+    CategoryController,
+    ContactController,
+    HomeController,
+    ProfileController,
+    TagController
+};
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeController::class)
-->name('home');
+Route::get('/', HomeController::class)->name('home');
 
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', fn() => view('dashboard'))
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])
-        ->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])
-        ->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])
-        ->name('profile.destroy');
+    Route::prefix('profile')->group(function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    Route::prefix('articles')->group(function () {
+        Route::get('create', [ArticleController::class, 'create'])->name('articles.create');
+        Route::post('create', [ArticleController::class, 'store'])->name('articles.store');
+        Route::get('{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
+        Route::patch('{article}', [ArticleController::class, 'update'])->name('articles.update');
+        Route::delete('{article}', [ArticleController::class, 'destroy'])->name('articles.destroy');
+    });
+
+    Route::prefix('tags')->group(function () {
+        Route::get('create', [TagController::class, 'create'])->name('tags.create');
+        Route::post('create', [TagController::class, 'store'])->name('tags.store');
+        Route::get('{tag}/edit', [TagController::class, 'edit'])->name('tags.edit');
+        Route::patch('{tag}', [TagController::class, 'update'])->name('tags.update');
+        Route::delete('{tag}', [TagController::class, 'destroy'])->name('tags.destroy');
+    });
+
+    Route::prefix('categories')->group(function () {
+        Route::get('create', [CategoryController::class, 'create'])->name('categories.create');
+        Route::post('store', [CategoryController::class, 'store'])->name('categories.store');
+        Route::get('{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+        Route::patch('{category}', [CategoryController::class, 'update'])->name('categories.update');
+        Route::delete('{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+    });
 });
 
-Route::get('/articles', [ArticleController::class, 'index'])
-    ->name('articles.index');
-
-Route::get('/articles/create', [ArticleController::class, 'create'])
-    ->middleware('auth')
-    ->name('articles.create');
-Route::post('/articles/create', [ArticleController::class, 'store'])
-    ->middleware('auth')
-    ->name('articles.store');
-
-Route::get('/articles/{article}', [ArticleController::class, 'show'])
-    ->name('articles.show');
-
-Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'])
-    ->middleware('auth')
-    ->name('articles.edit');
-
-Route::patch('/articles/{article}', [ArticleController::class, 'update'])
-    ->middleware('auth')
-    ->name('articles.update');
-
-Route::delete('/articles/{article}', [ArticleController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('articles.destroy');
-
-Route::get('/tags',
-        [TagController::class, 'index'])
-    ->name('tags.index');
-
-Route::get('/tags/create', [TagController::class, 'create'])
-    ->middleware('auth')
-    ->name('tags.create');
-Route::post('/tags/create', [TagController::class, 'store'])
-    ->middleware('auth')
-    ->name('tags.store');
-
-Route::get('/tags/{tag}/edit', [TagController::class, 'edit'])
-    ->middleware('auth')
-    ->name('tags.edit');
-
-Route::get('/tags/{tag}', [TagController::class, 'show'])
-    ->name('tags.show');
-
-Route::patch('/tags/{tag}', [TagController::class, 'update'])
-    ->middleware('auth')
-    ->name('tags.update');
-
-Route::delete('/tags/{tag}', [TagController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('tags.destroy');
-
-Route::get('/categories',
-        [CategoryController::class, 'index'])
-    ->name('categories.index');
-
-Route::patch('/categories/{category}', [CategoryController::class, 'update'])
-    ->middleware('auth')
-    ->name('categories.update');
-
-Route::get('/categories/create',
-    [CategoryController::class, 'create'])
-    ->middleware('auth')
-    ->name('categories.create');
-
-Route::get('/categories/{category}',
-        [CategoryController::class, 'show'])
-        ->name('categories.show');
-
-Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])
-    ->middleware('auth')
-    ->name('categories.edit');
-
-Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('categories.destroy');
-
-
-Route::post('/categories/store',
-       [CategoryController::class, 'store'])
-    ->name('categories.store');
+Route::resources([
+    'articles' => ArticleController::class,
+    'tags' => TagController::class,
+    'categories' => CategoryController::class,
+]);
 
 Route::get('/about', AboutController::class)->name('about');
-
-Route::get('/contact',
-    [ContactController::class, 'index'])
-    ->name('contact');
-
-Route::post('/contact',
-    [ContactController::class, 'submit'])
-    ->name('contact.submit');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
 
 require __DIR__.'/auth.php';
 
